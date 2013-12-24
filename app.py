f2fb70d457fd34e3eb005ef22bb18abccc8e3f47
Connor Peet <connor@connorpeet.com>
2013-12-24 17:40:36 -0600

    It works!

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, render_template, redirect, abort
+
+app = Flask(__name__);
+app.config.update(
+	DEBUG=True,
+	SECRET_KEY='changeme',
+	REDIS_HOST='localhost',
+	REDIS_PORT=6379,
+	REDIS_DB=0,
+
+	APP_HOST='0.0.0.0',
+	APP_PORT=5000,
+
+	URL_BASE = 'http://192.168.1.149:5000/' # Must end with a slash
+);
+
+import redis
+r = redis.StrictRedis(host=app.config['REDIS_HOST'], port=app.config['REDIS_PORT'], db=app.config['REDIS_DB'])
+
+@app.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
+def index(path):
+	count = r.hlen('links')
+	if request.method == 'GET':
+		return render_template('index.html', count=count, url_base=app.config['URL_BASE'])
+	else:
+		chars = []
+		while (count >= 0):
+			if count > 31:
+				chars.append(31)
+			else:
+				chars.append(count)
+
+			count -= 31
+
+		str = ''
+		for char in chars:
+			str += chr(101 + char)
+
+		r.hset('links', str, request.form['url'])
+		return str
+
+@app.route('/<link>')
+def get_link(link):
+	url = r.hget('links', link)
+
+	if url:
+		return redirect(url)
+	else:
+		return abort(404)
+
+
+app.run(host=app.config['APP_HOST'], port=app.config['APP_PORT'])
---
+++ static/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	$urlbox = $('#url');
+	$fg = $('.form-group');
+	$count = $('#count');
+	$btn = $('#submit');
+	baseUrl = $('#baseurl').html();
+
+	$btn.click(function() {
+		url = $urlbox.val();
+		$(this).addClass('disabled').attr('disabled', 'disabled');
+
+		$.ajax({
+			url: '/',
+			type: 'POST',
+			data: {url: url},
+		})
+		.done(function(data) {
+			$fg.addClass('has-success');
+			$urlbox.val(baseUrl + data);
+
+			$urlbox[0].select();
+			$count.html(parseInt($count.html(), 10) + 1);
+		})
+		.fail(function() {
+			$fg.addClass('has-error');
+			$urlbox.val('Something went wrong!');
+		})
+		.always(function() {
+			$btn.removeClass('disabled');
+			$btn.removeAttr('disabled');
+		});
+	});
+
+	$fg.click(function() {
+		$(this).removeClass('has-success has-error');
+	})
+});
